a5e0195bc5c6056797ae6bf0739f3e158e7d91cc
kangseong-sim <stylesha34@gmail.com>
2023-02-22 10:41:10 +0900

    🎨 design: 공동구매 글쓰기 UI

---
+++ client/src/pages/Write/styled.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const WriteWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const WriteContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Imeage = styled.div`
+  width: 175px;
+  height: 100px;
+  background-color: #fffdfd;
+  border: .1008px solid #d2cdcd;
+  border-radius: 10px;
+  margin: 1.875rem;
+`;
+
+export const SubmitBtn = styled.button`
+  width: 65px;
+  height: 20px;
+  margin: 0 0 1.875rem 16.875rem;
+  color: #ffffff;
+  font-size: .625rem;
+  background-color: #f9bb00;
+  border-radius: 5px;
+`;
+
+export const WriteForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  `;
+
+export const InputDiv = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+export const InputLabel = styled.div`
+  padding-right: 1.875rem;
+`;
+
+export const WriteInput = styled.input`
+  width: 106px;
+  border-bottom: .0625rem solid #999999;
+`;
+
+export const InputDivs = styled.div`
+  margin-right: 130px;
+  margin-bottom: 20px;
+`;

e0f05333ea43281eb4f7256a5410449be8838b97
kangseong-sim <stylesha34@gmail.com>
2023-02-22 10:30:38 +0900

    ✨ feat: app.tsx 초기 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./Style/globalStyle";
+import styled from "styled-components";
+import Main from "./pages/Main/Main";
+import Write from "./pages/Write/Write";
+import Footer from "./components/Footer/Footer";
 
-function App() {
+const Domain = styled.div`
+  min-width: 22.5rem;
+  min-height: 40rem;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        선택 과목이 함수는 돌연변이 함수가 실행되기 전에 실행되며 돌연변이 함수가 받는 것과 동일한 변수가 전달됩니다.돌연변이가 성공하기를 바라며 리소스에 대한 낙관적 업데이트를 수행하는 데 유용합니다.이 함수에서 반환된 값은 변형 실패 시 onError및 함수 모두에 전달되며 낙관적 업데이트를 롤백하는 데 유용할 수 있습니다.onSettled
-        </p>
-      </header>
-    </div>
+    <Domain>
+      <GlobalStyle />
+      {/* <Routes>
+        <Route path="/" element={<Main />} 
+        <Route path="/write" element={<Write />} 
+      </Routes> */}
+        <Write />
+        <Footer />
+    </Domain>
   );
 }
 

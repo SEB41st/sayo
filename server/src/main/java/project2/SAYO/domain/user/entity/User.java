2170c4f45e0cf5afe681719793a1951b7a2732f5
keumbi <rmaqldi@gmail.com>
2023-02-24 17:53:35 +0900

    ✨ feat : global

--- server/src/main/java/project2/SAYO/domain/user/entity/User.java
+++ server/src/main/java/project2/SAYO/domain/user/entity/User.java
@@ -12,7 +12,6 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
 public class User extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

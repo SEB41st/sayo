badade2f5928778d2a406c39d5846341a1a12a87
janghanna <ewal4291@gmail.com>
2023-02-27 11:49:34 +0900

    ✨ feat: mypage 구현

--- client/src/pages/Mypage/Mypage.tsx
+++ client/src/pages/Mypage/Mypage.tsx
@@ -1,21 +1,10 @@
 import * as S from "./styled";
 
-const Mypage = () => {
-  return (
-    <S.MypageWrap>
-      <S.MypageContainer>
-        <S.ImageDiv></S.ImageDiv>
-        <S.MypageDiv>
-          <span className="Name">이름</span>
-          <span className="UserName">장한나</span>
-        </S.MypageDiv>
-      </S.MypageContainer>
-      <S.ButtonDiv>
-        <S.LogoutBtn>로그아웃</S.LogoutBtn>
-        <S.LogoutBtn>회원탈퇴</S.LogoutBtn>
-      </S.ButtonDiv>
-    </S.MypageWrap>
-  );
-};
-
-export default Mypage;
+const MyPage = () => {
+    return (
+        <S.Main>
+마이페이지
+        </S.Main>
+    )
+}
+export default MyPage;

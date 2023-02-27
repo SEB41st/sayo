37b561a41fbe96a868711a7f0bf4969acf35704d
janghanna <ewal4291@gmail.com>
2023-02-27 23:41:57 +0900

    ✨ feat: 마이페이지 UI 구현

--- client/src/pages/MyPage/MyPage.tsx
+++ client/src/pages/MyPage/MyPage.tsx
@@ -1,29 +1,54 @@
 import * as S from "./styled";
-import { LogoImg, GoodsList, Item} from '../Main/styled';
+import { LogoImg, Line, Item} from '../Main/styled';
 import { Link } from "react-router-dom";
 
 const Mypage = () => {
   return (
     <S.MypageWrap>
-      <S.MypageContainer>
         <div className="Title">마이페이지</div>
-        <S.ImageDiv></S.ImageDiv>
-        <div>NICKNAME</div>
-      </S.MypageContainer>
-      <S.Lists>
-          <div>내가 찜한 상품</div>
-        <S.ChoiceList>
-            <Link to = '/detail'><Item/></Link>
-            <Link to = '/detail'><Item/></Link>
-            <Link to = '/detail'><Item/></Link>
-        </S.ChoiceList>
-          <div>참여 중인 공동구매</div>
-        <S.ChoiceList>
-            <Link to = '/detail'><Item/></Link>
-            <Link to = '/detail'><Item/></Link>
-            <Link to = '/detail'><Item/></Link>
-          </S.ChoiceList>
-      </S.Lists>
+        <S.MypageContainer>
+            <S.ImageDiv></S.ImageDiv>
+            <div className="Nickname">NICKNAME</div>
+        </S.MypageContainer>
+        <S.Line></S.Line>
+        <S.Lists>
+            <div className="Name">내가 찜한 상품</div>
+            <S.ChoiceList>
+                <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div>
+                </Link>
+                <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div></Link>
+                <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div></Link>
+            </S.ChoiceList>
+        </S.Lists>
+        <S.Line></S.Line>
+        <S.Lists>
+            <div className="Name">참여 중인 공동구매</div>
+            <S.ChoiceList>
+            <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div>
+                </Link>
+                <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div></Link>
+                <Link to = '/detail'>
+                    <Item src="/assets/goods.png"/>
+                    <div>목포 쫀드기</div>
+                    <div>9,900원</div></Link>
+            </S.ChoiceList>
+        </S.Lists>
+        <S.Line></S.Line>
     </S.MypageWrap>
   );
 };

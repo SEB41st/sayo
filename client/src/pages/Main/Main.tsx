4c15cacb56d0f9bf06a687f17a013e3a1527786b
janghanna <ewal4291@gmail.com>
2023-02-27 09:38:57 +0900

    ✨ feat: main page 구현

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,14 +1,55 @@
-import { Link } from 'react-router-dom'
-import * as S from './styled'
+import { Link } from 'react-router-dom';
+import * as S from './styled';
+import { BsPlusCircle, BsSearch } from 'react-icons/bs';
 
 const Main = () => {
   return (
     <S.Main>
-      Main
-      <Link to = "/write">공동구매 제안하기</Link> 
-
-
-
+      <S.LogoImg src="/assets/neighbors.jpeg" alt ="" ></S.LogoImg>
+      <S.MainList>
+        <S.Line/>
+        <S.Title>강남구 공동구매 상품</S.Title>
+          <S.Menus>
+            <h4 className='font'>전체보기
+              <Link to='/itemList' ><BsPlusCircle className='plusIcon'/></Link>
+            </h4>
+            <S.GoodsList>
+              <Link to = '/detail'><S.Item></S.Item></Link>
+              <Link to = '/detail'><S.Item></S.Item></Link>
+              <Link to = '/detail'><S.Item></S.Item></Link>
+              
+            </S.GoodsList>
+          </S.Menus>
+        <S.Title>지역 공동구매 상품 찾기</S.Title>
+        <S.Menus>
+          <S.SearchBar>
+          <input
+          className='Search'
+          type="text"
+          placeholder='검색하실 지역을 입력해주세요'
+          ></input>
+          <Link to = '/map'>
+          <BsSearch style={{"marginLeft":"10px"}}/>
+          </Link>
+          </S.SearchBar>
+        </S.Menus>
+            <S.Title>최근 본 상품</S.Title>
+        <S.Menus>
+          <S.GoodsList>
+            <Link to = '/detail'><S.Item></S.Item></Link>
+            <Link to = '/detail'><S.Item></S.Item></Link>
+            <Link to = '/detail'><S.Item></S.Item></Link>
+          </S.GoodsList>
+        </S.Menus>
+        <S.Menus>
+          <S.Title style={{margin:0}}>
+            찾으시는 제품이 없다면?
+            <Link to = "/write">
+              <S.WriteButton>공동구매 제안하기</S.WriteButton>
+            </Link> 
+          </S.Title>
+        </S.Menus>
+      </S.MainList>
     </S.Main>
   )
 }

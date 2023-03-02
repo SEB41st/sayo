50d4d598e27fba7ded59cb997d515649e7b35717
kangseong-sim <stylesha34@gmail.com>
2023-03-02 15:51:36 +0900

    ✨ feat: 클릭하는 위치에 마커 생성 구현

--- client/src/components/Map/MapMain.tsx
+++ client/src/components/Map/MapMain.tsx
@@ -1,5 +1,7 @@
 import * as S from "./styled";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
 
 // kakao 글로벌로 선언
 declare global {
@@ -8,24 +10,67 @@ declare global {
   }
 }
 
-const MapMain = () => {
-  useEffect(() => {
-    let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    let options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+interface Position {
+  lat: number;
+  lng: number;
+}
 
-    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+const MapMain = () => {
+  // 현재 마커 저장할 state
+  const [position, setPosition] = useState<Position[]>([]);
+
+
+  useEffect(() => {
+    mapscript();
   }, []);
 
-  return <div id="map" style={{ width: "500px", height: "500px" }} />;
-  // <S.MapWrap>
-  //   <S.MapContainer>Map</S.MapContainer>
-  //   </S.MapWrap>
+  const mapscript = () => {
+    let container = document.getElementById("map") as HTMLElement;
+    let options = {
+      center: new kakao.maps.LatLng(37.5668872688006, 126.97863243245928),
+      level: 5,
+    };
+    //map
+    const map = new kakao.maps.Map(container, options);
 
-  //   <S.Main>지도로 찾기</S.Main>;
+    let marker = new kakao.maps.Marker({ 
+      // 지도 중심좌표에 마커를 생성합니다 
+      position: map.getCenter() 
+  }); 
+  // 지도에 마커를 표시합니다
+  marker.setMap(map);
+
+  kakao.maps.event.addListener(map, 'click', function(mouseEvent: any) {        
+    
+    // 클릭한 위도, 경도 정보를 가져옵니다 
+    let latlng = mouseEvent.latLng; 
+    console.log(latlng)
+    // 현재 위치 저장
+    setPosition(latlng);
+    
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+    
+    let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+    
+
+    
+});
+  };
+
+
+
+  return (
+    <div id="map" style={{ width: '550px', height: '400px' }}>
+      {/* <Map center={{ lat: position.lat, lng: position.lng }}>
+      {markers.map((marker, index) => (
+        <MapMarker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+      ))}
+      </Map> */}
+    </div>
+  );
 };
 export default MapMain;
 

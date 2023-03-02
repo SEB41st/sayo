64b64330c648217bfa140d65f34eb7aa02ee88f9
kangseong-sim <stylesha34@gmail.com>
2023-03-02 11:07:18 +0900

    ✨ feat: 지도

---
+++ client/src/components/Map/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MapWrap = styled.div`
+  width: 90%;
+  height: 100vh;
+  margin: 0.625rem 1.25rem;
+`;
+
+export const MapContainer = styled.div`
+  width: 90%;
+  height: 100vh;
+  margin: 0.625rem 1.25rem;
+`;

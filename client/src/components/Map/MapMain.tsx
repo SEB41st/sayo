61bd59ff7bfa49edd24f8e9df46c285d2562ffbe
kangseong-sim <stylesha34@gmail.com>
2023-03-03 13:32:36 +0900

    ✨ feat: Main map 컴포넌트 구현

--- client/src/components/Map/MapMain.tsx
+++ client/src/components/Map/MapMain.tsx
@@ -1,8 +1,4 @@
-import * as S from "./styled";
-import { useEffect, useState } from "react";
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
-
-
+import { useEffect, useState } from "react"
 // kakao 글로벌로 선언
 declare global {
   interface Window {
@@ -15,11 +11,11 @@ interface Position {
   lng: number;
 }
 
-
 const MapMain = () => {
+  // 지도를 처음에 랜더링 해줌
+
   // 현재 마커 저장할 state
   const [position, setPosition] = useState<Position[]>([]);
-
 
   useEffect(() => {
     mapscript();
@@ -34,49 +30,35 @@ const MapMain = () => {
     //map
     const map = new kakao.maps.Map(container, options);
 
-    let marker = new kakao.maps.Marker({ 
-      // 지도 중심좌표에 마커를 생성합니다 
-      position: map.getCenter() 
-  }); 
-  // 지도에 마커를 표시합니다
-  marker.setMap(map);
+    let marker = new kakao.maps.Marker({
+      // 지도 중심좌표에 마커를 생성합니다
+      position: map.getCenter(),
+    });
+    // 지도에 마커를 표시합니다
+    marker.setMap(map);
 
-  kakao.maps.event.addListener(map, 'click', function(mouseEvent: any) {        
-    
-    // 클릭한 위도, 경도 정보를 가져옵니다 
-    let latlng = mouseEvent.latLng; 
-    console.log(latlng)
-    // 현재 위치 저장
-    setPosition(latlng);
-    
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-    
-    let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-    message += '경도는 ' + latlng.getLng() + ' 입니다';
-    
+    kakao.maps.event.addListener(map, "click", function (mouseEvent: any) {
+      // 클릭한 위도, 경도 정보를 가져옵니다
+      let latlng = mouseEvent.latLng;
+      console.log(latlng);
+      // 현재 위치 저장
+      setPosition(latlng);
 
-    
-});
+      // 마커 위치를 클릭한 위치로 옮깁니다
+      marker.setPosition(latlng);
+    });
   };
 
-
-
   return (
-    <div id="map" style={{ width: '550px', height: '400px' }}>
-      {/* <Map center={{ lat: position.lat, lng: position.lng }}>
+    <>
+      <div id="map" style={{ width: "550px", height: "400px" }}>
+        {/* <Map center={{ lat: position.lat, lng: position.lng }}>
       {markers.map((marker, index) => (
         <MapMarker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
-      ))}
+        ))}
       </Map> */}
-    </div>
+      </div>
+    </>
   );
 };
 export default MapMain;
-
-
-
-
-
-
-

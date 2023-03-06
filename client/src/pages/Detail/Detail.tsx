cd54beb4922cfc5706f595ea91b093e58175649b
kangseong-sim <stylesha34@gmail.com>
2023-03-06 16:27:27 +0900

    ✨ feat: Detail page 데이터 연결

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,32 +1,55 @@
 import { Link } from "react-router-dom";
 import * as S from "./styled";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { BsHeartFill } from "react-icons/bs";
+import { useCustomQuery } from "../../components/util/useCustomQuery";
+import { useParams } from "react-router-dom";
+import Loading from "../../components/Loading/Loading";
+import Error from "../../components/Error/Error";
+import MapMain from "../../components/Map/MapMain";
+import MapSalesLoaction from "../../components/Map/MapSalesLoaction";
+import MapLocation from "../../components/Map/MapLocation";
+
+export interface LatLng {
+  latitude: any;
+  longitude: any;
+}
 
 const Detail = () => {
+  // const [SalesLocation], setSalesLocation] = useState<LatLng[]>( latitude:"", longitude:"" );
+
+  const [SalesLocation, setSalesLocation] = useState< Array<any> >([]);
 
   const [value, SetValue] = useState(new Date());
-  useEffect(() => {
 
-    var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
-    
-    // 이미지 지도에 표시할 마커입니다
-    // 이미지 지도에 표시할 마커는 Object 형태입니다
-    var marker = {
-      position: markerPosition
-    };
-    
-    var staticMapContainer  = document.getElementById('staticMap') as HTMLElement, // 이미지 지도를 표시할 div  
-    staticMapOption = { 
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 이미지 지도의 중심좌표
-      level: 3, // 이미지 지도의 확대 레벨
-      marker: marker // 이미지 지도에 표시할 마커 
-    };    
-    
-    // 이미지 지도를 생성합니다
-    var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-    
-  },[])
+  const { itemId } = useParams();
+
+  const { data, isLoading, error, refetch } = useCustomQuery(
+    `/items/${itemId}`,
+    `items=${itemId}`
+  );
+
+
+  if (isLoading) return <Loading></Loading>;
+  if (error) return <Error></Error>;
+
+  const Items = data;
+
+  console.log(Items.location);
+
+  
+  // console.log(Items.location.Ma);
+
+
+
+  const location: any = Items.location;
+
+  // setSalesLocation(location)
+
+  // setSalesLocation(location);
+  // console.log(SalesLocation)
+  // const longitude: any = Items.location.Ma;
+
   return (
     <S.DetailWrap>
       <S.DetailContainer>
@@ -35,13 +58,15 @@ const Detail = () => {
         </S.ImageDiv>
         <S.ProductInfoDiv>
           <div className="Product">
-            <div className="ProductName">아이렌캐리어 네임택</div>
-            <BsHeartFill size="20" style={{marginLeft: "10px", color:"#d3d3d3"}}></BsHeartFill>
+            <div className="ProductName">{Items.title}</div>
+            <BsHeartFill
+              size="20"
+              style={{ marginLeft: "10px", color: "#d3d3d3" }}
+            ></BsHeartFill>
           </div>
-          <div className="ProductPrice">판매가 : 6800원</div>
-          <div className="ProductFee">배송비 : 없음</div>
-          <div className="SalesSchedule">
-              판매일정</div>
+          <div className="ProductPrice">판매가 : {Items.itmePrice}</div>
+          <div className="ProductFee">배송비 : {Items.deliveryFee}</div>
+          <div className="SalesSchedule">판매일정</div>
           <S.ButtonDiv>
             <S.CartBtn>
               <Link to="/cart">장바구니</Link>
@@ -54,12 +79,14 @@ const Detail = () => {
       </S.DetailContainer>
       <S.DetailDiv>
         <div className="DetailInfo">상세정보</div>
-        <div className="DetailInfoTxt">
-          깜찍한 아이렌 캐릭터 러기지 네임텍 입니다 설레는 여행을 함께 할 귀여운
-          아이렌 형태의 러기지 네임텍 입니다!
-        </div>
+        <div className="DetailInfoTxt">{Items.itemDatail}</div>
         <div className="DetailLocation">위치</div>
-        <S.StaticMap id='staticMap'/>
+        <MapMain></MapMain>
+        {/* <MapSalesLoaction  setSalesLocation={setSalesLocation} /> */}
+         
+        {/* </MapSalesLoaction> */}
+        <MapLocation></MapLocation>
+        {/* <S.StaticMap id="staticMap" /> */}
       </S.DetailDiv>
     </S.DetailWrap>
   );

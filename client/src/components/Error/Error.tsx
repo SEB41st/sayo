229df94957fbc56bbfbdd08691c6af005e0c9990
kangseong-sim <stylesha34@gmail.com>
2023-03-06 16:22:27 +0900

    ✨ feat: error 컴포넌트 생성

---
+++ client/src/components/Error/Error.tsx
@@ -0,0 +1,24 @@
+import * as S from "./styled";
+
+const Error = () => {
+  return (
+    <S.ErrorDiv>
+      <S.ErrorText>
+        <img
+          src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+          alt="404"
+        />
+        <span>404 error</span>
+        <p className="p-a">): Whoops </p>
+        <p className="p-b">
+          It seems like we couldn't find the page you were looking for
+        </p>
+        <a href="/" className="back">
+          HOME
+        </a>
+      </S.ErrorText>
+    </S.ErrorDiv>
+  );
+};
+
+export default Error;

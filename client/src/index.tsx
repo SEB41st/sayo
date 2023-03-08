836c8f91c60cc8621a968b3dddf913c8e76f31a7
kangseong-sim <stylesha34@gmail.com>
2023-03-08 10:16:55 +0900

    ✨ feat: 클라이언트 상태관리 위한 recooil 초기 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Style/globalStyle.ts';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Style/globalStyle.ts";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 declare global {
   interface Window {
@@ -18,25 +16,27 @@ declare global {
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      //다른 창을 갔다 왔을 떄 api콜을 다시 해줄 것인지(최신화를 시켜줄 것인지)에 대한 옵션 
+      //다른 창을 갔다 왔을 떄 api콜을 다시 해줄 것인지(최신화를 시켜줄 것인지)에 대한 옵션
       refetchOnWindowFocus: false,
       //api 요청에서 error 발생 시 다시 요청을 보내 주는 옵션 (3번 정도 다시 보내 줌)
       retry: false,
-      //캐시 결과를 먼저 확인하지 않고 api 요청을 보낼 것인지에 대한 옵션 
+      //캐시 결과를 먼저 확인하지 않고 api 요청을 보낼 것인지에 대한 옵션
       refetchOnMount: false,
     },
   },
 });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-  );
-  root.render(
-    <QueryClientProvider client={queryClient}>
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
       {/* <React.StrictMode> */}
-        <Router>
-          <App />
-        </Router>
+      <Router>
+        <App />
+      </Router>
       {/* </React.StrictMode> */}
-    </QueryClientProvider>
-  );
+    </RecoilRoot>
+  </QueryClientProvider>
+);

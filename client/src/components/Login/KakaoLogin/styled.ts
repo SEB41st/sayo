218402a4db02dc75f7fdd1e352f9258317533136
janghanna <ewal4291@gmail.com>
2023-03-10 14:26:15 +0900

    ✨ feat: naver,kakao 소셜 로그인 구현(css 추가 수정 예정)

---
+++ client/src/components/Login/KakaoLogin/styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import KaKaoLogin from 'react-kakao-login';
+
+export const StKaKaoLogin = styled.div`
+    cursor: pointer;
+    /* border-radius:10px; */
+    /* width: 200px; */
+    /* &:hover{
+        box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0.2), 0 0px 20px 0 rgba(0, 0, 0, 0.19);
+    } */
+`;
+
+export const KaKaoBtn = styled(KaKaoLogin)`
+    padding: 0;
+    width: 190px;
+    height: 44px;
+    line-height: 44px;
+    color: #783c00;
+    background-color: #FFEB00;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+    &:hover{
+        box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2)
+    }
+`

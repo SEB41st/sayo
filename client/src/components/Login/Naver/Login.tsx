2b13aeb5207ad0322d7c9fb801aee8f5d6ffd23a
janghanna <ewal4291@gmail.com>
2023-03-13 16:23:50 +0900

    ✨ feat: naver 소셜로그인 커스터마이징

--- client/src/components/Login/Naver/Login.tsx
+++ client/src/components/Login/Naver/Login.tsx
@@ -3,12 +3,12 @@ import * as S from "./styled";
 
 const Login = () => {
       
-    const naverRef = useRef()
+    const naverRef = useRef<any>()
 	const { naver } = window as any
 	const NAVER_CLIENT_ID = `${process.env.REACT_APP_NAVER_CLIENT_ID}`// 발급 받은 Client ID 입력 
 	const NAVER_CALLBACK_URL = `${process.env.REACT_APP_NAVER_CALLBACK_URL}`// 작성했던 Callback URL 입력
 
-    console.log(`${process.env.REACT_APP_NAVER_CLIENT_ID}`)
+    // console.log(`${process.env.REACT_APP_NAVER_CLIENT_ID}`)
 	const initializeNaverLogin = () => {
 		const naverLogin = new naver.LoginWithNaverId({
             clientId: NAVER_CLIENT_ID,
@@ -31,22 +31,19 @@ const Login = () => {
            // 백엔드 개발자가 정보를 전달해준다면 아래 요기! 라고 작성된 부분까지는 
            // 코드 생략이 가능하다.  
       
-      naverLogin.getLoginStatus(async function (status:any) {
+        naverLogin.getLoginStatus(async function (status:any) {
 			if (status) {
               // 아래처럼 선택하여 추출이 가능하고, 
 				const userid = naverLogin.user.getEmail()
 				const username = naverLogin.user.getName()
 
-                console.log("naver로그인",naverLogin.user)
+                console.log("naver로그인",naverLogin)
               // 정보 전체를 아래처럼 state 에 저장하여 추출하여 사용가능하다. 
-              // setUserInfo(naverLogin.user)
+            //   setUserInfo(naverLogin.user)
 			}
 		})     
             // 요기!
 	}
-    
-    
-    
             // 네이버 소셜 로그인 (네아로) 는 URL 에 엑세스 토큰이 붙어서 전달된다.
             // 우선 아래와 같이 토큰을 추출 할 수 있으며,
             // 3부에 작성 될 Redirect 페이지를 통해 빠르고, 깨끗하게 처리가 가능하다.
@@ -56,28 +53,32 @@ const Login = () => {
 	}
         
       	const getToken = () => {
-		const token = window.location.href.split('=')[1].split('&')[0]
+		    const token = window.location.href.split('=')[1].split('&')[0]
              // console.log, alert 창을 통해 토큰이 잘 추출 되는지 확인하자! 
         		console.log(token)
              // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!   
                 // localStorage.setItem('access_token', token)
 		        // setGetToken(token)
-	}
-
-        
+	    }
              // 화면 첫 렌더링이후 바로 실행하기 위해 useEffect 를 사용하였다.
-	useEffect(() => {
-		initializeNaverLogin()
-		userAccessToken()
-	}, [])
-    {/* 구현할 위치에 아래와 같이 코드를 입력해주어야 한다. 
+        useEffect(() => {
+            initializeNaverLogin()
+            userAccessToken()
+        }, [])
+        {/* 구현할 위치에 아래와 같이 코드를 입력해주어야 한다. 
          태그에 id="naverIdLogin" 를 해주지 않으면 오류가 발생한다! */}
 
-        //  const handleNaverLogin = () => {
-        //     naverRef.current.children[0].click()
-        // }
+         const handleNaverLogin = () => {
+            console.log(naverRef.current.children[0])
+            naverRef.current.children[0].click()
+        }
+
+    //     const REST_API_KEY = `${process.env.REACT_APP_KAKAO_API_KEY}`
+    //     // console.log(REST_API_KEY)
+    // const REDIRECT_URI = "http://localhost:3000/Login"
+    // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 	return (
-		<div>
+		<S.Login>
             <S.LoginTitle>
 			    <span className='title'>로그인</span>
                 <span className='subtitle'>로그인 후 다양한 상품들을 구매해보세요 !</span>
@@ -86,20 +87,26 @@ const Login = () => {
                 {/* <S.NaverBtn id="naverIdLogin"/> */}
                 <S.NaverIdLogin 
                     id="naverIdLogin"
-                  
+                    ref={naverRef}
                 />
-			<S.NaverLoginBtn>
-				{/* <NaverIcon alt="navericon" /> */}
-				<S.NaverLoginTitle>네이버로 로그인</S.NaverLoginTitle>
-			</S.NaverLoginBtn>
+                <S.NaverLoginBtn onClick={handleNaverLogin}>
+                    <S.NaverIcon/>
+                    <S.NaverLoginTitle>네이버로 로그인하기</S.NaverLoginTitle>
+                </S.NaverLoginBtn>
                 <S.KaKaoBtn
                     token={`${process.env.REACT_APP_KAKAO_API_KEY}`}
                     onSuccess={console.log}
                     onFail={console.error}
                     onLogout={console.info}
                     />
+                <S.KakaoLoginBtn>
+                    <S.KakaoIcon/>
+                    {/* <a href={KAKAO_AUTH_URL}>카카오로 로그인하기</a> */}
+                    {/* <S.NaverLoginTitle>카카오로 로그인하기</S.NaverLoginTitle> */}
+                </S.KakaoLoginBtn>
             </div>
-		</div>
+            {/* <img src="/assets/NaverIcon.png"></img> */}
+		</S.Login>
 	)
 }
 
--- client/src/components/Login/Naver/styled.ts
+++ client/src/components/Login/Naver/styled.ts
@@ -31,8 +31,26 @@ export const NaverBtn = styled.div`
 
 export const KaKaoBtn = styled(KaKaoLogin)`
     padding: 0;
-    width: 190px;
-    height: 44px;
+    width: 10px;
+    height: 30%;
+    line-height: 44px;
+    color: #783c00;
+    background-color: #fff;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    font-size: 10px;
+    font-weight: bold;
+    text-align: center;
+    margin: 20px;
+    cursor: pointer;    
+    &:hover{
+        box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2)
+    }
+`
+export const KakaoLoginBtn = styled.div`
+    padding: 0;
+    width: 28vh;
+    height: 30%;
     line-height: 44px;
     color: #783c00;
     background-color: #FFEB00;
@@ -42,11 +60,12 @@ export const KaKaoBtn = styled(KaKaoLogin)`
     font-weight: bold;
     text-align: center;
     margin: 20px;
-    cursor: pointer;
+    cursor: pointer;    
     &:hover{
         box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2)
     }
 `
+
 export const NaverIdLogin = styled.div`
 	display: none;
 `
@@ -54,26 +73,34 @@ export const NaverIdLogin = styled.div`
 export const NaverLoginBtn = styled.button`
 	display: flex;
 	align-items: center;
-	width: 360px;
-	height: 56px;
+	width: 28vh;
+	height: 30%;
 	background-color: #03c75a;
 	border-radius: 6px;
 `
 
 // 로그인 버튼 사용가이드 링크를 들어가면 이미지를 받아 이렇게 적용이 가능하다 ! 
-// const NaverIcon = styled.div`
-// 	width: 30px;
-// 	height: 30px;
-// 	margin-left: 10px;
-// 	background: url('/images/Login/navericon.png') no-repeat center;
-// 	background-size: 30px;
-// `
+export const NaverIcon = styled.div`
+	width: 30px;
+	height: 30px;
+	margin-left: 10px;
+	background: url("/assets/NaverIcon.png") no-repeat center;
+	background-size: 30px;
+`
+
+export const KakaoIcon = styled.div`
+	width: 30px;
+	height: 30px;
+	margin-left: 10px;
+	background: url("/assets/kakaotalk.png") no-repeat center;
+	background-size: 30px;
+`
 
 export const NaverLoginTitle = styled.span`
-	margin-left: 90px;
+	margin-left: 20px;
 	color: ${({ theme }) => theme.White};
 	font-weight: 400;
-	font-size: 14px;
+	font-size: 13px;
 	line-height: 24px;
 `
 
@@ -84,9 +111,23 @@ export const LoginTitle = styled.div`
     .title{
         margin: 10px;
         font-size: 30px;
+        @media only screen and (max-width: ${"400px"}) {
+            margin: 10px;
+            font-size: 15px;
+        }
     }
     .subtitle{
         margin: 10px;
         font-size: 20px;
+        @media only screen and (max-width: ${"400px"}) {
+            margin: 10px;
+            font-size: 10px;
+        }
     }
+`
+export const Login = styled.div`
+    margin: 20%;
+    padding: 5%;
+    background-color: aliceblue;
+    border-radius: 10px;
 `

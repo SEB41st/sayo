0004d168f42cf20dda717fd028d4e54bf81e92fe
kangseong-sim <stylesha34@gmail.com>
2023-03-14 14:44:01 +0900

    🎨 design: 구글 로그인 버튼 커스텀

---
+++ client/src/components/Login/Google/styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const GoogleLoginWrapper = styled.div`
+  display: flex;
+  /* width: 300px; */
+  align-items: center;
+  justify-content: space-around;
+  width: 28vh;
+  height: 30%;
+  background-color: #ffffff;
+  border-radius: 6px;
+  font-size: 13px;
+  cursor: pointer;
+
+  .logo{
+    width: 30px;
+    height: 30px;
+  }
+`;

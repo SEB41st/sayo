c67488051901852cd7a5585709f2029f8f1ae99c
janghanna <ewal4291@gmail.com>
2023-03-14 13:32:28 +0900

    🎨 design: main slick css변경 작업 진행 중

--- client/src/components/ItemsSlider/styled.ts
+++ client/src/components/ItemsSlider/styled.ts
@@ -3,18 +3,46 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     position: relative;
-    width: 300px;
-    height: 80px;
+    width: 90%;
     .item{
-        margin-right: 20px;
+        margin: 20px;
         flex-direction: column;
         width: 100vh;
-        height: 100px;
+        height: 100vw;
     }
+    .dots_custom {
+    display: flex;
+    justify-content: center;
+    
+  }
+  
+  .dots_custom li {
+    /* list-style: none; */
+    cursor: pointer;
+    display: inline-block;
+    margin: 0 6px;
+    padding: 0;
+  }
+  
+  .dots_custom li button {
+    border: none;
+    background: #d1d1d1;
+    color: transparent;
+    cursor: pointer;
+    display: block;
+    height: 8px;
+    width: 8px;
+    border-radius: 100%;
+    padding: 0;
+  }
+  
+  .dots_custom li.slick-active button {
+    background-color: #F9BB00;
+  }
 `
 export const Item = styled.div`
-    max-height: 80px;
-    max-width: 80px;
+    height: 50px;
+    max-width: 50%;
     border-radius: 10px;
     margin: 10px;
     gap: 30px;
@@ -24,11 +52,15 @@ export const Item = styled.div`
     .itempicture{
         max-height: 80%;
         max-width: 80%;
+        margin: 20px;
         :hover{
         transform: scale(1.2);
     }
+
     }
     @media screen and (min-width: ${"1280px"}) {
       
     }
+
+
 `

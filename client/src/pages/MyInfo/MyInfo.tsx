98513efd67083e589ee14653a3823d7a24c6f4a2
janghanna <ewal4291@gmail.com>
2023-03-23 17:33:22 +0900

    ✨ feat:회원 주소 및 주소 수정 버튼 생성

--- client/src/pages/MyInfo/MyInfo.tsx
+++ client/src/pages/MyInfo/MyInfo.tsx
@@ -4,11 +4,16 @@ const MyInfo = () => {
     return (
         <S.MypageWrap>
         <S.MypageContainer>
-          <S.ImageDiv></S.ImageDiv>
+          <S.ImageDiv src="/assets/Github.png"></S.ImageDiv>
           <S.MypageDiv>
             <span className="Name">이름</span>
             <span className="UserName">장한나</span>
           </S.MypageDiv>
+          <S.MypageDiv>
+            <span className="Name">주소</span>
+            <span className="address">서울특별시 영등포구 0000로 00길 0000아파트 101동 101호 </span>
+          </S.MypageDiv>
+          <S.LogoutBtn>주소수정</S.LogoutBtn>
         </S.MypageContainer>
         <S.ButtonDiv>
           <S.LogoutBtn>로그아웃</S.LogoutBtn>

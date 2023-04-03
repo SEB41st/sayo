5835c2fc8363d03d5f4dd90e68805b52993d25fa
kangseong-sim <stylesha34@gmail.com>
2023-04-03 16:17:50 +0900

    ✨ feat: 이미지 업로드 style

---
+++ client/src/components/ModifyImage/styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ModifyImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  
+  label{
+    cursor: pointer;
+  }
+
+  input{
+    display: none;
+
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;

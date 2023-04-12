4ec67add616cf08cfdba028e55ad33a98db15d1a
김희진 <khj4209kk@gmail.com>
2023-04-12 10:58:14 +0900

    ✨ feat: wishCount 추가

--- server/src/main/java/project2/SAYO/domain/item/repository/ItemRepository.java
+++ server/src/main/java/project2/SAYO/domain/item/repository/ItemRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project2.SAYO.domain.item.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Item findItemById(long itemId);
 }

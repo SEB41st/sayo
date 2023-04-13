5e743fb165a1730e2fd9e88997e4bfb53a26f9ec
janghanna <ewal4291@gmail.com>
2023-04-13 16:21:16 +0900

    ✨ feat: item wish 기능 추가

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -17,41 +17,101 @@ import { useRecoilState } from "recoil";
 import axios from "axios";
 import { useCustomMutation } from "../../components/util/useMutation";
 
+
 export interface LatLng {
   latitude: any;
   longitude: any;
 }
 
+interface WishItem {
+  createdAt: string;
+  itemId: number;
+  itemName: string;
+  itemPicture: string;
+  itemPrice: number;
+  modifiedAt: string;
+  userId: number;
+  wishId: number;
+  wishSelected: boolean;
+  Id: number;
+}
+
+const apiCall = async (url: any) => {
+  return await axios
+    .get(`http://sayo.n-e.kr:8080${url}`, {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        Accept: "application/json",
+        AutHorization: localStorage.getItem("accessToken"),
+      },
+    })
+    .then((res) => res.data.data)
+    .catch((error) => {
+      if (error) return false;
+    });
+};
+
 const Detail = () => {
   const [modalOpen, SetModalOpen] = useState<boolean>(false);
-  const [like, setLike] = useRecoilState(likeState);
+  // const [like, setLike] = useRecoilState(likeState);
   const [addCart, setAddCart] = useState<boolean>(false);
-  const [wish, setwishId] = useState<number>(0)
-  // const userId = localStorage.getItem("userId")
+  // const [wish, setwishId] = useState<number>(0)
+  // // const userId = localStorage.getItem("userId")
 
-  const { itemId } = useParams();
+  // const { itemId } = useParams();
 
-  useEffect(() => {
-    axios
-      .get(`http://sayo.n-e.kr:8080/wishes/${wish}`,
-      {
+  // useEffect(() => {
+  //   axios
+  //     .get(`http://sayo.n-e.kr:8080/wishes/${wish}`,
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json;charset=UTF-8",
+  //         Accept: "application/json",
+  //         "AutHorization" : localStorage.getItem("accessToken"),
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   }, [wish]);
+  const [like, setLike] = useState<boolean>(false);
+  // const [like, setLike] = useRecoilState(likeState);
+  const [item, setItem] = useState<WishItem[]>([]);
+  const [wish, setWish] = useState<WishItem[]>([]);
+
+  const { Id } = useParams();
+  console.log(Id)
+  const userId = localStorage.getItem("userId")
+
+    useEffect(() => {
+      axios
+      (`http://sayo.n-e.kr:8080/wishes/user/${userId}/wish`, {
+        method: "get",
         headers: {
-          "Content-Type": "application/json;charset=UTF-8",
-          Accept: "application/json",
-          "AutHorization" : localStorage.getItem("accessToken"),
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("accessToken"),
         },
       })
-      .then((res) => {
-        console.log(res)
+      .then((res: any) => {
+        setWish(res.data.data);
+        refetch();
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
       });
-    }, [wish]);
+  }, []);
+
+  const hasItemId = wish.some(item => item.itemId === Number(Id));
+  console.log("hasItemId", hasItemId)
+  console.log("wish", wish)
+  
 
   const { data, isLoading, error, refetch } = useCustomQuery(
-    `/items/get/${itemId}`,
-    `/items/get/=${itemId}`
+    `/items/get/${Id}`,
+    `/items/get/=${Id}`
   );
 
   // const { mutate } = useCustomMutation(
@@ -59,11 +119,16 @@ const Detail = () => {
   //   `/wishes/${itemId}`,
   //   "POST"
   // );
+  const { mutate } = useCustomMutation(
+    `/wishes/${Id}`,
+    `/wishes/${Id}`,
+    "POST"
+  );
   // const { mutate } = useCustomMutation(`/shoppingCarts/items/${itemId}`, `/shoppingCarts/items/${itemId}`, "POST");
 
   const PostCart = async () => {
     await axios
-      (`http://sayo.n-e.kr:8080/shoppingCarts/items/${itemId}`, {
+      (`http://sayo.n-e.kr:8080/shoppingCarts/items/${Id}`, {
         method: "post",
         headers: {
           "Content-Type": "application/json",
@@ -82,17 +147,34 @@ const Detail = () => {
       });
   };
 
+  const handleLikeBtn = () => {
+    // if (!isLogin) return navigate("/login");
+    axios
+      (`http://sayo.n-e.kr:8080/wishes/${Id}`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("accessToken"),
+        },
+      })
+      .then((res) => {
+        setLike(!res.data.data.wishSelected);
+        // window.location.reload();
+        // refetch();
+        // console.log(res.data.data.wishSelected);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   if (isLoading) return <Loading></Loading>;
   if (error) return <Error></Error>;
 
   const Items = data.data;
 
-  console.log(Items);
-
-  const location: any = Items.location;
+  // const location: any = Items.location;
   // console.log(location);
-
-
 
   // setSalesLocation(location)
   // const longitude: any = Items.location.Ma;
@@ -107,32 +189,33 @@ const Detail = () => {
   // const handleLikeBtn = () => {
   //   setLike(!like);
   //   mutate(like);
+  //   // window.location.reload();
+  //   refetch();
   // };
 
-  // const handlePostCart = () => {
-  //   // setLike(!like)
-  //   mutate(openModal)
-  // }
+  
 
-  const handleLikeBtn = () => {
-    // if (!isLogin) return navigate("/login");
-    axios
-      (`http://sayo.n-e.kr:8080/wishes/${itemId}`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.getItem("accessToken"),
-        },
-      })
-      .then((res) => {
-        setLike(!like);
-        setwishId(res.data.data.wishId)
-        refetch()
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+ 
+
+  // const handleLikeBtn = () => {
+  //   // if (!isLogin) return navigate("/login");
+  //   axios
+  //     (`http://sayo.n-e.kr:8080/wishes/${itemId}`, {
+  //       method: "post",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: localStorage.getItem("accessToken"),
+  //       },
+  //     })
+  //     .then((res) => {
+  //       setLike(!like);
+  //       setwishId(res.data.data.wishId)
+  //       refetch()
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
   function CommaFormat(x:any) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
@@ -147,8 +230,10 @@ const Detail = () => {
         </S.ImageDiv>
         <S.ProductInfoDiv>
           <div className="Product">
+          
             <div className="ProductName">{Items.itemName}</div>
-            {Items.wishSelected ? (
+            
+            { hasItemId ? (
               <BsHeartFill
                 onClick={handleLikeBtn}
                 size="20"

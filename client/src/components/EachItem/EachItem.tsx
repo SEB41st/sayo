0910bfdf803564ab072b468df61c3c54a7e8f3ee
kangseong-sim <stylesha34@gmail.com>
2023-04-20 20:51:03 +0900

    ✨ feat: 페이지네이션 위한 props 수정

--- client/src/components/EachItem/EachItem.tsx
+++ client/src/components/EachItem/EachItem.tsx
@@ -1,32 +1,32 @@
-import * as S from "./Styled"
+import * as S from "./Styled";
 
-const EachItem = (item:any) => {
-    // console.log(item.item)
-    return(
+const EachItem = (items: any) => {
+  return (
     <S.EachItem>
-        {item.item.itemStatus === "ITEM_END" ? 
-          <>
-          <S.EndImg> 판매 종료 </S.EndImg> 
-            <S.Item>
-            <img src={item.item.itemPicture} alt="goods"></img>
-            </S.Item> 
-            <S.Font>
-              <div>{item.item.itemName}</div>
-              <div>{item.item.itmePrice}</div>
-            </S.Font>
-          </>
-           : 
-          <>
+      {items.items.itemStatus === "ITEM_END" ? (
+        <>
+          <S.EndImg> 판매 종료 </S.EndImg>
           <S.Item>
-            <img src={item.item.itemPicture} alt="goods"></img>
-          </S.Item> <S.Font>
-            <div>{item.item.itemName}</div>
-            <div>{item.item.itmePrice}</div>
+            <img src={items.items.itemPicture} alt="goods"></img>
+          </S.Item>
+          <S.Font>
+            <div>{items.items.itemName}</div>
+            <div>{items.items.itmePrice}</div>
           </S.Font>
-          </>
-          }
-    </S.EachItem>)
-
-}
+        </>
+      ) : (
+        <>
+          <S.Item>
+            <img src={items.items.itemPicture} alt="goods"></img>
+          </S.Item>{" "}
+          <S.Font>
+            <div>{items.items.itemName}</div>
+            <div>{items.items.itmePrice}</div>
+          </S.Font>
+        </>
+      )}
+    </S.EachItem>
+  );
+};
 
 export default EachItem;

086e7ecf0f69d239ccebf29fa6d580bc15aa60a5
kangseong-sim <stylesha34@gmail.com>
2023-04-24 11:41:05 +0900

    ✨ feat: 내가 찜한 공동구매 상품 목록 이름 변경

---
+++ client/src/pages/ItemList/MyWishItemList.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import * as S from "./styled";
+import { LogoImg, Line } from "../Main/styled";
+import { useCustomQuery } from "../../components/util/useCustomQuery";
+import Loading from "../../components/Loading/Loading";
+import Error from "../../components/Error/Error";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import EachItem from "../../components/EachItem/EachItem";
+
+const MyWishItemList = () => {
+  const userId = localStorage.getItem("userId");
+
+  const { data, isLoading, error, refetch } = useCustomQuery(
+    `/wishes/user/${userId}/wish`,
+    `wishes/user/${userId}/wish`
+  );
+
+
+  if (isLoading) return <Loading></Loading>;
+  if (error) return <Error></Error>;
+
+  const Items = data.data;
+
+  return (
+    <S.Main>
+      <LogoImg src="/assets/event.png" alt=""></LogoImg>
+      <S.MainList>
+        <Line />
+
+        <S.Title>찜한 상품 목록</S.Title>
+        <S.GoodsList>
+          {Items &&
+            Items.map((items: any) => (
+              <Link to={`/detail/${items.itemId}`} key={items.itemId}>
+                <EachItem items={items} />
+              </Link>
+            ))}
+        </S.GoodsList>
+
+      </S.MainList>
+    </S.Main>
+  );
+};
+
+export default MyWishItemList;

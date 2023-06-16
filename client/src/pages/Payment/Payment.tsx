f55a569f721f957f25b361b17c0bd5a3d1db14ed
janghanna <ewal4291@gmail.com>
2023-06-16 11:59:39 +0900

    ♻️ refactor: useCustomQuery로 리펙토링 및 주문자 정보 표시

--- client/src/pages/Payment/Payment.tsx
+++ client/src/pages/Payment/Payment.tsx
@@ -30,44 +30,28 @@ declare global {
 
 
 const Cart = () => {
-  const [user, setUser] = useState()
-  const [orderId, setOrderId] = useState('');
 
-  console.log(orderId)
-  console.log(user)
-  // console.log(items)
   const userId = localStorage.getItem("userId")
-
-  useEffect(() => {
-    axios(`http://sayo.n-e.kr:8080/users/${userId}/mypage`, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("accessToken"),
-      },
-    })
-      .then((res: any) => {
-        console.log(res.data.data)
-        setUser(res.data.data)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
 
   const CommaFormat = (x: any) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
-  const {data, isLoading, error} = useCustomQuery(
+  const {data, refetch, isLoading} = useCustomQuery(
     `/shoppingCarts/order/user/${userId}`,
     `/shoppingCarts/order/user/${userId}`
   )
-  if (isLoading) return <Loading></Loading>;
-  if (error) return <Error></Error>;
-  const Items = data.data;
-  console.log(Items[0].itemName)
+  
+  const {data:User,isLoading:UserIsLoading, error,refetch:UserRefetch} = useCustomQuery(
+    `/users/${userId}/mypage`,
+    `/users=${userId}`
+    )
+    if (isLoading) return <Loading></Loading>;
+    if (UserIsLoading) return <Loading></Loading>;
+    if (error) return <Error></Error>;
+    refetch();
+    UserRefetch();
+    const Items = data.data;
   let totalPrice = 0;
   let totalDeliverPrice = 0;
 
@@ -94,9 +78,7 @@ const Cart = () => {
         }
       })
       .then((res) => {
-        setOrderId(res.data.data.orderId)
-        handleTossPay();
-        console.log(res.data.data)
+        handleTossPay(res.data.data.orderId);
       })
       .catch((err) => {
         console.log(err);
@@ -107,7 +89,7 @@ const Cart = () => {
         // 2. 결제창 SDK 초기화
         var tossPayments = window.TossPayments(clientKey)
 
-        const handleTossPay  = () => {
+        const handleTossPay  = (orderId:String) => {
           tossPayments.requestPayment('카드', {
             amount: totalPrice+totalDeliverPrice,
             orderId: orderId,
@@ -125,8 +107,71 @@ const Cart = () => {
     <S.CartWrap>
       <S.CartContainer>
         <div className="Cart">주문/결제</div>
+        {Items.length === 0 ?
         <S.PaymentDiv>
-        <S.ProductDiv>
+          {
+            User.data.addressList.length === 0 || 
+            User.data.addressList[0].addressUserName === "" || 
+            User.data.addressList[0].phoneNumber === ""||
+            User.data.addressList[0].roadAddress === "" ?
+          <S.UserDiv>
+            <h4>주문자 정보</h4>
+            <div>상단 프로필 사진 클릭 또는 "내 정보"탭에서 배송 정보를 입력해주세요</div>
+          </S.UserDiv>:
+          <S.UserDiv>
+            <h4>주문자 정보</h4>
+            <div>주문자 : {User.data.addressList[0].addressUserName}</div>
+            <div>주소지 : {User.data.addressList[0].roadAddress}detailAddress</div>
+            <div>연락처 : {User.data.addressList[0].phoneNumber}</div>
+          </S.UserDiv> }
+         <S.ProductDiv>
+          
+            <S.ProductInfoDiv>
+            <S.ProductInfoDiv2>
+              <div className="Name"></div>
+              <div className="ProductFee">수량 : 0개</div>
+              <div className="Price">제품 가격 : 0원</div>
+            </S.ProductInfoDiv2>
+          </S.ProductInfoDiv>
+        
+        </S.ProductDiv>
+
+        <S.TotalDiv>결제 예정 금액</S.TotalDiv>
+        
+        <S.TotalPriceDiv>
+          <S.TotalPriceDiv2>
+            <S.TotalPriceDiv3>상품금액</S.TotalPriceDiv3>
+            <S.TotalPriceDiv3>0원</S.TotalPriceDiv3>
+          </S.TotalPriceDiv2>
+          <S.TotalPriceDiv2>
+            <S.TotalPriceDiv3>배송비</S.TotalPriceDiv3>
+            <S.TotalPriceDiv3>0원</S.TotalPriceDiv3>
+          </S.TotalPriceDiv2>
+          <S.TotalPriceDiv2>
+            <S.TotalPriceDiv3>결제 예정 금액</S.TotalPriceDiv3>
+            <S.TotalPriceDiv3>0원</S.TotalPriceDiv3>
+          </S.TotalPriceDiv2>
+        </S.TotalPriceDiv>
+        
+        <S.ButtonDiv2>
+        <CartBtn style={{"color":"gray"}}>결제하기</CartBtn>
+        </S.ButtonDiv2>
+        </S.PaymentDiv> : <S.PaymentDiv>
+        {User.data.addressList.length === 0 || 
+        User.data.addressList[0].addressUserName === "" || 
+        User.data.addressList[0].phoneNumber === ""||
+        User.data.addressList[0].roadAddress === "" ?
+        <S.UserDiv>
+        <h4>주문자 정보</h4>
+        <div>상단 프로필 사진 클릭 또는 "내 정보"탭에서 배송 정보를 입력해주세요</div>
+      </S.UserDiv>:
+        <S.UserDiv>
+            <h4>주문자 정보</h4>
+            <span><strong>주문자</strong>: {User.data.addressList[0].addressUserName}</span>
+            <span><strong>주소지</strong> : {User.data.addressList[0].roadAddress} {User.data.addressList[0].detailAddress}</span>
+            <span><strong>연락처</strong> : {User.data.addressList[0].phoneNumber}</span>
+          </S.UserDiv>}
+         <S.ProductDiv>
           {Items && Items.map((item:any) => (
             <S.ProductInfoDiv>
               <div>
@@ -157,11 +202,17 @@ const Cart = () => {
             <S.TotalPriceDiv3>{CommaFormat(totalPrice+totalDeliverPrice)}원</S.TotalPriceDiv3>
           </S.TotalPriceDiv2>
         </S.TotalPriceDiv>
-        
+        {User.data.addressList.length !== 0 &&
+        User.data.addressList[0].addressUserName !== "" &&
+        User.data.addressList[0].phoneNumber !== "" &&
+        User.data.addressList[0].roadAddress !== "" ?
         <S.ButtonDiv2>
           <CartBtn onClick={handlePay}>결제하기</CartBtn>
-        </S.ButtonDiv2>
+        </S.ButtonDiv2> : <S.ButtonDiv2>
+          <CartBtn style={{"color":"gray"}}>결제하기</CartBtn>
+        </S.ButtonDiv2>}
         </S.PaymentDiv>
+        }
       </S.CartContainer>
     </S.CartWrap>
   );

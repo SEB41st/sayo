e587d0523d5c2b00e78389d87e47d899bf517ee9
janghanna <ewal4291@gmail.com>
2023-06-22 11:22:53 +0900

    💄 style: 결제 성공 후 success/fail 화면 UI 수정

--- client/src/pages/Payment/success.tsx
+++ client/src/pages/Payment/success.tsx
@@ -1,10 +1,18 @@
 import axios from "axios";
+import moment from "moment";
 import { useEffect } from "react";
 import * as S from "./styled";
 
 const Success = () => {
     const Url:any = window.location.href;
     const url = new URL(Url);
+
+    let today = new Date();
+    console.log(today)
+    let payYear = moment().format('YYYY')
+    let payMonth = moment().format('MM')
+    let payDay = moment().format('DD')
+
 
 const urlParams = url.searchParams;
     useEffect(() => {
@@ -35,8 +43,21 @@ return (
     <S.Fail>
         <S.FailText>
         <span>결제 완료</span>
+        <h2>{urlParams.get('amount')}원</h2>
+        <S.payInfo>
+          <span className="title">
+            <div>주문 금액</div>
+            <div>결제 수단</div>
+            <div>결제 일시</div>
+          </span>
+          <span>
+            <div>{urlParams.get('amount')}원</div>
+            <div>토스페이/카드</div>
+            <div>{payYear}년 {payMonth}월 {payDay}일</div>
+          </span>
+        </S.payInfo>
             <p>결제가 완료되었습니다</p>
-        <a href='/' className="back">홈으로 돌아가기</a>
+        <a href='/' className="back">HOME</a>
         </S.FailText>
     </S.Fail>
 

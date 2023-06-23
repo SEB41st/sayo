8aff32bf42b03258de436237cae1e0f5fbbd7605
김희진 <khj4209kk@gmail.com>
2023-06-23 21:24:42 +0900

    ✨ feat: orderResponse 에러 수정

--- server/src/main/java/project2/SAYO/domain/order/dto/OrderResponseDto.java
+++ server/src/main/java/project2/SAYO/domain/order/dto/OrderResponseDto.java
@@ -1,13 +1,10 @@
 package project2.SAYO.domain.order.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor

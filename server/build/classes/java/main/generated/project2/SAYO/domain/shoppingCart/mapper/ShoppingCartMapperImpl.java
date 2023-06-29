48102fad679d3aa03ee1e16ab98eb425f98e77ff
김희진 <khj4209kk@gmail.com>
2023-06-29 10:40:21 +0900

    ✨ feat: order 조회

---
+++ server/build/classes/java/main/generated/project2/SAYO/domain/shoppingCart/mapper/ShoppingCartMapperImpl.java
@@ -0,0 +1,13 @@
+package project2.SAYO.domain.shoppingCart.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-27T11:03:22+0900",
+    comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class ShoppingCartMapperImpl implements ShoppingCartMapper {
+}
---
+++ server/build/classes/java/main/generated/project2/SAYO/domain/wish/mapper/WishMapperImpl.java
@@ -0,0 +1,13 @@
+package project2.SAYO.domain.wish.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-27T11:03:23+0900",
+    comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class WishMapperImpl implements WishMapper {
+}

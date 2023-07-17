782ea6eabf09930166f7dfab4fc67c9e971b3bdd
janghanna <ewal4291@gmail.com>
2023-07-17 11:51:53 +0900

    ✨ feat: 재가빙 기능 추가

--- client/src/components/Rejoin/Rejoin.tsx
+++ client/src/components/Rejoin/Rejoin.tsx
@@ -1,15 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { useCustomMutation } from "../util/useMutation";
 import * as S from "./styled";
 
+
 const Rejoin = () => {
+  const Url:any = window.location.href;
+  const url = new URL(Url);
+  const urlParams = url.searchParams;
+  const userId = urlParams.get("userId")
+  const navigate = useNavigate()
+
+  const handleRejoin = () => {
+    mutate({})
+};
     const { mutate } = useCustomMutation(
-        `/users/`,
-        `users=`,
-        "POST", {
-          onSuccess: (result:any) => {
-            console.log(result); // 성공한 뒤의 결과 값 출력
-            // 추가적인 로직 수행
-          }
+        `/users/${userId}`,
+        `users=${userId}`,
+        "POST", 
+        (res:any) => {
+          toast("재가입이 완료되었습니다");
+          navigate("/login");
         }
       );
 
@@ -21,7 +32,7 @@ return (
         <p className="p-a">): Whoops </p>
         <p className="p-b">탈퇴된 계정입니다.</p>
         <a href='/' className="back">HOME</a>
-        <a href='/login' className="back">재가입 후 로그인하기</a>
+        <button onClick={handleRejoin} className="back">재가입 후 로그인하기</button>
         </S.ErrorText>
     </S.Error>
 

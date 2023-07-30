1a63c0add655964287ee9c247a6b45f0562c9099
김희진 <khj4209kk@gmail.com>
2023-07-30 23:55:26 +0900

    ✨ feat: Address/User 관계 재설정 및 payment 내 UserName을 Address의 Name으로 변경

--- server/src/main/java/project2/SAYO/domain/address/repository/AddressRepository.java
+++ server/src/main/java/project2/SAYO/domain/address/repository/AddressRepository.java
@@ -3,5 +3,8 @@ package project2.SAYO.domain.address.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project2.SAYO.domain.address.entity.Address;
 
+import java.util.Optional;
+
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    Optional<Address> findByUserId(long userId);
 }
